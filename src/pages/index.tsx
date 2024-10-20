5f8e52dd4191359558fdca1564eb92778e89627b
spde3289 <spde3289@naver.com>
2024-10-20 20:55:04 +0900

    chore: tsconfig.json 절대경로 변경

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <div> 메인 페이지 </div>
+      <div> 메인 페이지 </div>
+      <div> 메인 페이지 </div>
+      <div> 메인 페이지 </div>
+      <div> 메인 페이지 </div>
+    </>
+  );
+}
