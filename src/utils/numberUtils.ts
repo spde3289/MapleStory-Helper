0b8faac852b3c20da7bd3b603f84050303f3cd32
spde3289 <spde3289@naver.com>
2024-12-14 21:03:01 +0900

    fix: 예상 시간 계산 로직 수정

--- src/utils/numberUtils.ts
+++ src/utils/numberUtils.ts
@@ -26,3 +26,42 @@ export const formatToEokUnit = (number: number): number => {
   const eok = Math.floor(number / 100000000) // 억 단위 계산
   return eok
 }
+
+export const convertTime = (units: number): string => {
+  const weeksInMonth = 4
+  const monthsInYear = 12
+
+  const months = Math.floor(units / weeksInMonth)
+  const remainingWeeks = units % weeksInMonth
+
+  const years = Math.floor(months / monthsInYear)
+  const remainingMonths = months % monthsInYear
+
+  let result = ''
+  if (years > 0) result += `${years}년 `
+  if (remainingMonths > 0) result += `${remainingMonths}달 `
+  if (remainingWeeks > 0) result += `${Math.ceil(remainingWeeks)}주`
+
+  return result.trim()
+}
+
+interface RewardResult {
+  need_darkness: number
+  bossReward: number
+  blackMageReward: number
+  totalReward: number
+  date: number
+}
+
+export const calculateRewards = (result: RewardResult): RewardResult => {
+  const reward = { ...result }
+
+  while (reward.totalReward < reward.need_darkness) {
+    reward.totalReward += reward.bossReward
+    if (reward.date % 4 === 0 || reward.date === 0) {
+      reward.totalReward += reward.blackMageReward
+    }
+    reward.date += 1
+  }
+  return reward
+}
