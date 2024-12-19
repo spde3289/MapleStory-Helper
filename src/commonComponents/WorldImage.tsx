1d08b04c9fa7c3084ecd1bce3f1de710c0051561
spde3289 <spde3289@naver.com>
2024-12-20 06:33:42 +0900

    feat: 서버이름 변경 리부트 => 에오스, 핼리오스

--- src/commonComponents/WorldImage.tsx
+++ src/commonComponents/WorldImage.tsx
@@ -8,16 +8,15 @@ interface CharacterImagePropsType {
 
 const WorldImage = ({ world_name }: CharacterImagePropsType) => {
   const worldSrc = (worldName: string): string => {
-    const reboot: string[] = ['리부트', '리부트2']
     const burning: string[] = ['버닝', '버닝2', '버닝3']
 
-    if (reboot.includes(worldName)) return '리부트'
     if (burning.includes(worldName)) return '버닝'
     return worldName
   }
 
   return (
     <Image
+      unoptimized={world_name === '에오스' || world_name === '핼리오스'}
       src={worldIcons[worldSrc(world_name)]}
       width={14}
       height={14}
