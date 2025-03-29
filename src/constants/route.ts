a9248711d170c1c00cb02086c8b295a1d8c80ff0
spde3289 <spde3289@naver.com>
2025-03-29 17:59:19 +0900

    feat: 재획 가계부 메인페이지 네이게이션 추가, 메인페이지 네비게이션 디자인 수정

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -1,15 +1,24 @@
+export type ItemType = {
+  name: string
+  path: string
+  description: string
+}
+
 export const calculatorItems = [
   {
     path: '/gem',
-    name: '주보 수익 계산기',
+    name: '주보 수익',
+    description: '내 주보 수익 캐릭터별로 싹 정리해드려요',
   },
   {
     path: '/genesis',
-    name: '제네시스 해방 퀘스트 계산기',
+    name: '제네시스 해방 퀘스트',
+    description: '언제 해방될지 날짜로 딱 알려드려요',
   },
   {
     path: '/destiny',
-    name: '데스티니 해방 퀘스트 계산기',
+    name: '데스티니 초월 퀘스트',
+    description: '초월까지 얼마나 남았는지 바로 확인해보세요',
   },
 ]
 
@@ -17,5 +26,6 @@ export const ledgerItems = [
   {
     path: '/jaehoik-log',
     name: '재획 가계부',
+    description: '오늘 얼마나 벌었는지 한눈에 확인해보세요',
   },
 ]
