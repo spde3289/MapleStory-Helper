fd7ddcb8b56b451046d1815e7e2e90b4ab4fea6e
spde3289 <spde3289@naver.com>
2026-01-12 02:20:31 +0900

    chore: 주석 오타 수정

--- src/types/error/apiErrors.ts
+++ src/types/error/apiErrors.ts
@@ -4,7 +4,7 @@ import { ApiError } from '@/lib/nexonApi/nexonClient'
 
 export type ApiErrorPayload = Record<string, unknown>
 
-// 서버 에러 타입
+// 넥슨 서버 에러 타입
 export type SeverErrorType =
   (typeof SERVER_ERROR_TYPES)[keyof typeof SERVER_ERROR_TYPES]
 
@@ -12,5 +12,5 @@ export type SeverErrorType =
 export type ClientErrorType =
   (typeof CLIENT_ERROR_TYPES)[keyof typeof CLIENT_ERROR_TYPES]
 
-// 사버 에러 응답 타입
+// 서버 에러 응답 타입
 export type ApiErrorResponse = ReturnType<ApiError['toJSON']>
