50ad6ff58954ede49ffd8df9360bb83b4d52fdcb
spde3289 <spde3289@naver.com>
2026-01-12 01:12:39 +0900

    feat: 공지사항 응답 타입, 요청 함수

--- src/constants/mapleEndpoints.ts
+++ src/constants/mapleEndpoints.ts
@@ -1,11 +1,30 @@
 export const MAPLE_ENDPOINTS = {
-  character: {
-    ocid: '/v1/id', // 캐릭터 식별자(ocid) 조회
-    list: '/v1/character/list', // 캐릭터 목록 조회
-    basic: '/v1/character/basic', // 기본 정보
-    stat: '/v1/character/stat', // 종합 능력치
+  CHARACTER: {
+    OCID: '/v1/id', // 캐릭터 식별자(ocid) 조회
+    LIST: '/v1/character/list', // 캐릭터 목록 조회
+    BASIC: '/v1/character/basic', // 기본 정보
+    STAT: '/v1/character/stat', // 종합 능력치
   },
-  user: {
-    raider: '/v1/user/union-raider', // 유니온 공격대 정보 조회
+  USER: {
+    RAIDER: '/v1/user/union-raider', // 유니온 공격대 정보 조회
+  },
+} as const
+
+export const MAPLESTORY_NOTICE_API = {
+  NOTICE: {
+    LIST: '/v1/notice',
+    DETAIL: '/v1/notice/detail',
+  },
+  UPDATE: {
+    LIST: '/v1/notice-update',
+    DETAIL: '/v1/notice-update/detail',
+  },
+  EVENT: {
+    LIST: '/v1/notice-event',
+    DETAIL: '/v1/notice-event/detail',
+  },
+  CASHSHOP: {
+    LIST: '/v1/notice-cashshop',
+    DETAIL: '/v1/notice-cashshop/detail',
   },
 } as const
---
+++ src/lib/nexonApi/noticeApi.ts
@@ -0,0 +1,98 @@
+import { MAPLESTORY_NOTICE_API } from '@/constants/mapleEndpoints'
+import {
+  CashshopNoticeDetailResponse,
+  CashshopNoticeListResponse,
+  EventNoticeDetailResponse,
+  EventNoticeListResponse,
+  NoticeDetailResponse,
+  NoticeListResponse,
+  UpdateNoticeListResponse,
+} from '@/types/api/notice'
+import { nexonClient } from './nexonClient'
+
+/** 일반 공지 리스트 */
+export const getNoticeList = async (): Promise<NoticeListResponse> => {
+  const response = await nexonClient.get<NoticeListResponse>(
+    MAPLESTORY_NOTICE_API.NOTICE.LIST,
+  )
+
+  return response.data
+}
+
+/** 일반 공지 상세보기 */
+export const getDetailNotice = async (
+  notice_id: string,
+): Promise<NoticeDetailResponse> => {
+  const response = await nexonClient.get<NoticeDetailResponse>(
+    MAPLESTORY_NOTICE_API.NOTICE.DETAIL,
+    { params: { notice_id } },
+  )
+
+  return response.data
+}
+
+/** 업데이트 공지 리스트 */
+export const getUpdateNoticeList =
+  async (): Promise<UpdateNoticeListResponse> => {
+    const response = await nexonClient.get<UpdateNoticeListResponse>(
+      MAPLESTORY_NOTICE_API.UPDATE.LIST,
+    )
+
+    return response.data
+  }
+
+/** 업데이트 공지 상세보기 */
+export const getUpdateDetailNotice = async (
+  notice_id: string,
+): Promise<NoticeDetailResponse> => {
+  const response = await nexonClient.get<NoticeDetailResponse>(
+    MAPLESTORY_NOTICE_API.UPDATE.DETAIL,
+    { params: { notice_id } },
+  )
+
+  return response.data
+}
+
+/** 이벤트 공지 리스트 */
+export const getEventNoticeList =
+  async (): Promise<EventNoticeListResponse> => {
+    const response = await nexonClient.get<EventNoticeListResponse>(
+      MAPLESTORY_NOTICE_API.EVENT.LIST,
+    )
+
+    return response.data
+  }
+
+/** 이벤트 공지 상세보기 */
+export const getEventDetailNotice = async (
+  notice_id: string,
+): Promise<EventNoticeDetailResponse> => {
+  const response = await nexonClient.get<EventNoticeDetailResponse>(
+    MAPLESTORY_NOTICE_API.EVENT.DETAIL,
+    { params: { notice_id } },
+  )
+
+  return response.data
+}
+
+/** 캐시샵 공지 리스트 */
+export const getCashshopNoticeList =
+  async (): Promise<CashshopNoticeListResponse> => {
+    const response = await nexonClient.get<CashshopNoticeListResponse>(
+      MAPLESTORY_NOTICE_API.CASHSHOP.LIST,
+    )
+
+    return response.data
+  }
+
+/** 캐시샵 공지 상세보기 */
+export const getCashshopDetailNotice = async (
+  notice_id: string,
+): Promise<CashshopNoticeDetailResponse> => {
+  const response = await nexonClient.get<CashshopNoticeDetailResponse>(
+    MAPLESTORY_NOTICE_API.CASHSHOP.DETAIL,
+    { params: { notice_id } },
+  )
+
+  return response.data
+}
