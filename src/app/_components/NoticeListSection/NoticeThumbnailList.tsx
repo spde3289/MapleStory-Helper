1a23269b96bafb63926edaaf302a3da41a2ee46d
spde3289 <spde3289@naver.com>
2026-01-20 16:14:32 +0900

    feat: 이벤트, 캐시샵 영역 디자인 수정

--- src/app/_components/NoticeListSection/NoticeThumbnailList.tsx
+++ src/app/_components/NoticeListSection/NoticeThumbnailList.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { Swiper, SwiperSlide } from 'swiper/react'
 import styles from './Carousel.module.css'
 
 import { ThumbnailNoticeSimple } from '@/app/_utils/mapper'
@@ -8,9 +7,6 @@ import { NoticeType } from '@/types/domain/notice'
 import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useEffect, useRef } from 'react'
-import { Autoplay, FreeMode, Mousewheel, Pagination } from 'swiper/modules'
-import type { Swiper as SwiperType } from 'swiper/types'
 
 interface Props {
   title: string
@@ -19,81 +15,35 @@ interface Props {
 }
 
 const NoticeThumbnailList = ({ title, type, noticeList }: Props) => {
-  const paginationRef = useRef<HTMLDivElement | null>(null)
-  const swiperRef = useRef<SwiperType | null>(null)
-
-  useEffect(() => {
-    if (!swiperRef.current || !paginationRef.current) return
-
-    const swiper = swiperRef.current
-
-    const paginationParams =
-      typeof swiper.params.pagination === 'object'
-        ? swiper.params.pagination
-        : {}
-
-    swiper.params.pagination = {
-      ...paginationParams,
-      el: paginationRef.current,
-      clickable: true,
-    }
-
-    swiper.pagination.destroy()
-    swiper.pagination.init()
-    swiper.pagination.render()
-    swiper.pagination.update()
-  }, [])
-
   return (
     <div className={clsx(styles.pagination, 'w-full aspect-auto')}>
-      <h3 className="font-medium text-zinc-500 text-sm">{title}</h3>
-      <Swiper
-        onSwiper={(swiper) => {
-          swiperRef.current = swiper
-        }}
-        navigation={true}
-        className={clsx(styles.root)}
-        spaceBetween={16}
-        freeMode={true}
-        slidesPerView={3}
-        loop
-        mousewheel
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          el: paginationRef.current,
-          clickable: true,
-        }}
-        modules={[Mousewheel, Pagination, Autoplay, FreeMode]}
-      >
+      <h3 className="font-bold text-sm mb-2">{title}</h3>
+      <div className="grid grid-cols-3 sm:grid-cols-4 gap-x-4 gap-y-2 text-xs">
         {noticeList.map((notice) => (
-          <SwiperSlide
-            key={notice.title}
-            className="text-sm hover:underline h-fit"
+          <Link
+            key={notice.noticeId}
+            href={ROUTES.BOARD.DETAIL(type, notice.noticeId)}
+            className="flex flex-col group flex-1 border rounded-md overflow-hidden border-gray-300 dark:border-neutral-700"
           >
-            <Link
-              href={ROUTES.BOARD.DETAIL(type, notice.noticeId)}
-              className="flex flex-col group"
-            >
-              <div className="relative aspect-video rounded-md overflow-hidden ">
-                <Image
-                  src={notice.thumbnail_url}
-                  fill
-                  className="object-cover group-hover:scale-110 transition-transform duration-300 ease-out"
-                  alt={notice.title}
-                />
-              </div>
-              <span className="flex-1 truncate">{notice.title}</span>
+            <div className="relative aspect-video overflow-hidden">
+              <Image
+                src={notice.thumbnail_url}
+                fill
+                quality={100}
+                unoptimized
+                className="object-cover group-hover:scale-110 transition-transform duration-300 ease-out"
+                alt={notice.title}
+              />
+            </div>
+            <div className="p-2">
+              <p className="flex-1 truncate font-bold ">{notice.title}</p>
               <span className="text-gray-500 shrink-0 text-xs truncate text-center">
                 {notice.text}
               </span>
-            </Link>
-          </SwiperSlide>
+            </div>
+          </Link>
         ))}
-      </Swiper>
-      <div ref={paginationRef} />
+      </div>
     </div>
   )
 }
