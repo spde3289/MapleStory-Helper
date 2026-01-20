b4b68650ff10193561bb059b99fe30f84b08c82f
spde3289 <spde3289@naver.com>
2026-01-20 15:42:59 +0900

    feat: 공지 리스트 페이지네이션 제거

--- src/app/_components/NoticeListSection/NoticePlainList.tsx
+++ src/app/_components/NoticeListSection/NoticePlainList.tsx
@@ -7,9 +7,6 @@ import { ROUTES } from '@/constants/routers/appRoutes'
 import { NoticeType } from '@/types/domain/notice'
 import clsx from 'clsx'
 import Link from 'next/link'
-import { useEffect, useRef } from 'react'
-import { Autoplay, Mousewheel, Pagination } from 'swiper/modules'
-import type { Swiper as SwiperType } from 'swiper/types'
 
 interface Props {
   title: string
@@ -18,53 +15,14 @@ interface Props {
 }
 
 const NoticePlainList = ({ title, type, noticeList }: Props) => {
-  const paginationRef = useRef<HTMLDivElement | null>(null)
-  const swiperRef = useRef<SwiperType | null>(null)
-
-  useEffect(() => {
-    if (!swiperRef.current || !paginationRef.current) return
-
-    const swiper = swiperRef.current
-
-    const paginationParams =
-      typeof swiper.params.pagination === 'object'
-        ? swiper.params.pagination
-        : {}
-
-    swiper.params.pagination = {
-      ...paginationParams,
-      el: paginationRef.current,
-      clickable: true,
-    }
-
-    swiper.pagination.destroy()
-    swiper.pagination.init()
-    swiper.pagination.render()
-    swiper.pagination.update()
-  }, [])
-
   return (
     <div className={clsx(styles.pagination, 'w-full')}>
-      <h3 className="font-medium text-zinc-500 text-sm">{title}</h3>
+      <h3 className="font-medium text-zinc-500 text-sm mb-2">{title}</h3>
       <Swiper
-        onSwiper={(swiper) => {
-          swiperRef.current = swiper
-        }}
-        className={clsx(styles.root, 'h-32')}
+        className={clsx(styles.root)}
         direction={'vertical'}
         spaceBetween={2}
         slidesPerView={'auto'}
-        loop
-        mousewheel
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          el: paginationRef.current,
-          clickable: true,
-        }}
-        modules={[Mousewheel, Pagination, Autoplay]}
       >
         {noticeList.map((notice) => (
           <SwiperSlide key={notice.title} className="text-sm hover:underline">
@@ -78,7 +36,6 @@ const NoticePlainList = ({ title, type, noticeList }: Props) => {
           </SwiperSlide>
         ))}
       </Swiper>
-      <div ref={paginationRef} />
     </div>
   )
 }
